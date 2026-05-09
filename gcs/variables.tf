3916163fce3aadf19f9c5a890a1be83ff03d898d
Marcelo Luiz <87508684+marcelo-luizz@users.noreply.github.com>
2026-05-09 10:33:14 -0300

    Create variables.tf

---
+++ gcs/variables.tf
@@ -0,0 +1,10 @@
+variable "bucket_name" {
+  type = string
+  description = "Bucket name"
+}
+
+variable "region" {
+  type = string
+  description = "Bucket Region"
+  default = "southamerica-east1"
+}
